f440714d7270784d75187d39ca53c0110c17ee60
Marcin Wróbel <marcinwrobel1986@protonmail.com>
2021-02-09 17:04:18 +0100

    #57: - added github action - moved dockerfile and scripts - made test working inside container

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 /bin/sh /lbaf/venv/bin/activate
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 for var in "$@"
 do
     eval "$var"
