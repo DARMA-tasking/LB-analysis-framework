8d2bddd8aff7847fe63034804baa6c2e7fc88af6
Marcin Wróbel <marcinwrobel1986@protonmail.com>
2021-12-30 13:13:12 +0100

    #123: - try changes on the test

--- scripts/test_lbaf.py
+++ scripts/test_lbaf.py
@@ -46,29 +46,18 @@ import sys
 
 import bcolors
 
-# Provide tests commands to TEST_COMMANDS.
-# Last word in the command should be the output directory. Then inbalance is checked.
-TESTS_COMMANDS = [
-    ["export PYTHONPATH=/lbaf/venv/lib/python3.8/site-packages:/lbaf:/lbaf/src:/lbaf/utils;", "python", "/lbaf/src/Applications/LBAF.py"],
-]
-
 
 def run_tests():
-    for test_cmd in TESTS_COMMANDS:
-        script = subprocess.run(test_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-        print(script.stdout)
-        if script.stderr:
-            raise RuntimeError(f"Commands: {test_cmd}\nSTDERR: {script.stderr}")
-        imbalance_file = f"{test_cmd[-1]}/imbalance.txt"
-        with open(imbalance_file, 'r') as imb_file:
-            imb_level = float(imb_file.read())
-            if imb_level < 100:
-                print(f"{bcolors.OKMSG}PASSED!\n=> TEST for INPUT {' '.join(test_cmd)}{bcolors.END}")
-                print(f"{bcolors.OKMSG}------------------------------------------------------------{bcolors.END}")
-            else:
-                print(f"{bcolors.ERR}FAILED!\n=> TEST for INPUT {' '.join(test_cmd)}{bcolors.END}")
-                print(f"{bcolors.ERR}------------------------------------------------------------{bcolors.END}")
-                sys.exit(1)
+    imbalance_file = f"/lbaf/output/imbalance.txt"
+    with open(imbalance_file, 'r') as imb_file:
+        imb_level = float(imb_file.read())
+        if imb_level < 100:
+            print(f"{bcolors.OKMSG}PASSED!\n=> TEST {bcolors.END}")
+            print(f"{bcolors.OKMSG}------------------------------------------------------------{bcolors.END}")
+        else:
+            print(f"{bcolors.ERR}FAILED!\n=> TEST{bcolors.END}")
+            print(f"{bcolors.ERR}------------------------------------------------------------{bcolors.END}")
+            sys.exit(1)
 
 
 if __name__ == "__main__":
