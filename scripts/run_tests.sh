df244bc924716d6da322c4f0349bd17b17066e63
Marcin Wróbel <marcinwrobel1986@protonmail.com>
2021-12-30 13:02:29 +0100

    #123: - try changes on the test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export PYTHONPATH=/lbaf/venv/lib/python3.8/site-packages:/lbaf:/lbaf/src
+export PYTHONPATH=/lbaf/venv/lib/python3.8/site-packages:/lbaf:/lbaf/src:/lbaf/utils
 export DISPLAY=:99.0
 /bin/sh /lbaf/venv/bin/activate
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
