9efdc02ec579dddf88d2ca79b0491ba1ca6e9590
Marcin Wróbel <marcinwrobel1986@protonmail.com>
2022-02-01 09:37:07 +0100

    #154: - added `terminal_background` option in conf.yaml - fixed added more than one handler

--- src/Utils/logger.py
+++ src/Utils/logger.py
@@ -10,24 +10,14 @@ from src.Utils.colors import red, green, blue, cyan, magenta, yellow, white, bla
 
 LOGGING_LEVEL = {'DEBUG': logging.DEBUG, 'INFO': logging.INFO, 'WARNING': logging.WARNING, 'ERROR': logging.ERROR}
 
-FORMATER_EXTENDED = {
-        logging.DEBUG: yellow("%(levelname)s [%(module)s.%(funcName)s()] ") + light_white("msg:[%(message)s]"),
-        logging.INFO: green("%(levelname)s [%(module)s.%(funcName)s()] ") + light_white("msg:[%(message)s]"),
-        logging.WARNING: cyan("%(levelname)s [%(module)s.%(funcName)s()] ") + light_white("msg:[%(message)s]"),
-        logging.ERROR: red("%(levelname)s [%(module)s.%(funcName)s()] ") + light_white("msg:[%(message)s]"),
-    }
-
-FORMATER_PPP = {
-        logging.DEBUG: yellow("[%(module)s] ") + light_white("%(message)s"),
-        logging.INFO: green("[%(module)s] ") + light_white("%(message)s"),
-        logging.WARNING: cyan("[%(module)s] ") + light_white("%(message)s"),
-        logging.ERROR: red("[%(module)s] ") + light_white("%(message)s"),
-    }
-
 
 class CustomFormatter(Formatter):
+    def __init__(self, frmttr):
+        super(CustomFormatter, self).__init__()
+        self.frmttr = frmttr
+
     def format(self, record):
-        log_fmt = FORMATER_PPP.get(record.levelno)
+        log_fmt = self.frmttr.get(record.levelno)
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
 
@@ -43,11 +33,31 @@ def logger(name: str = 'root'):
         ll = loggin_level.upper()
     else:
         raise KeyError('No logging_level key in config file!')
+    terminal_bg = conf.get('terminal_background', None)
+    if terminal_bg == 'light':
+        clr_fnc = black
+    else:
+        clr_fnc = light_white
+    FORMATER_EXTENDED = {
+        logging.DEBUG: yellow("%(levelname)s [%(module)s.%(funcName)s()] ") + clr_fnc("msg:[%(message)s]"),
+        logging.INFO: green("%(levelname)s [%(module)s.%(funcName)s()] ") + clr_fnc("msg:[%(message)s]"),
+        logging.WARNING: cyan("%(levelname)s [%(module)s.%(funcName)s()] ") + clr_fnc("msg:[%(message)s]"),
+        logging.ERROR: red("%(levelname)s [%(module)s.%(funcName)s()] ") + clr_fnc("msg:[%(message)s]"),
+    }
+
+    FORMATER_PPP = {
+        logging.DEBUG: yellow("[%(module)s] ") + clr_fnc("%(message)s"),
+        logging.INFO: green("[%(module)s] ") + clr_fnc("%(message)s"),
+        logging.WARNING: cyan("[%(module)s] ") + clr_fnc("%(message)s"),
+        logging.ERROR: red("[%(module)s] ") + clr_fnc("%(message)s"),
+    }
 
     lgr = logging.getLogger(name)
     lgr.setLevel(LOGGING_LEVEL.get(ll))
-    ch = logging.StreamHandler()
-    ch.setLevel(LOGGING_LEVEL.get(ll))
-    ch.setFormatter(CustomFormatter())
-    lgr.addHandler(ch)
+    if not lgr.hasHandlers():
+        ch = logging.StreamHandler()
+        ch.setLevel(LOGGING_LEVEL.get(ll))
+        ch.setFormatter(CustomFormatter(frmttr=FORMATER_PPP))
+        lgr.addHandler(ch)
+
     return lgr
