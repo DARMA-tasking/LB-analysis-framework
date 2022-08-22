10d817deb25970e1c038e3ca51d7e45c58eed33e
Marcin Wróbel <marcin.wrobel@ng-analytics.com>
2022-08-22 15:51:23 +0200

    #277: - fixed unit test

--- tests/test_configuration_validator.py
+++ tests/test_configuration_validator.py
@@ -78,7 +78,7 @@ class TestConfig(unittest.TestCase):
 
         with self.assertRaises(SchemaError) as err:
             ConfigurationValidator(config_to_validate=configuration, logger=logger()).main()
-        self.assertEqual(err.exception.args[0], "Should be of type 'list' of 'int' types")
+        self.assertEqual(err.exception.args[0], "Should be of type 'list' of 'int' types\nShould be of type 'str' like '0-100'")
 
     def test_config_validator_wrong_from_data_phase_name(self):
         with open(os.path.join(self.config_dir, 'conf_wrong_from_data_phase_name.yml'), 'rt') as config_file:
