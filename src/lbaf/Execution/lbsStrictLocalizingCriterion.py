4c6b53addaa2d04c8e443fa432fbd25f58e20a3e
Philippe P. Pébaÿ <philippe.pebay@ng-analytics.com>
2022-12-31 20:21:38 +0100

    #331: updated and fixed strict localizing criterion

--- src/lbaf/Execution/lbsStrictLocalizingCriterion.py
+++ src/lbaf/Execution/lbsStrictLocalizingCriterion.py
@@ -6,8 +6,7 @@ from ..Model.lbsRank import Rank
 
 
 class StrictLocalizingCriterion(CriterionBase):
-    """ A concrete class for a strictly localizing criterion
-    """
+    """ A concrete class for a strictly localizing criterion."""
     
     def __init__(self, workmodel, lgr):
         """ Class constructor."""
@@ -16,32 +15,32 @@ class StrictLocalizingCriterion(CriterionBase):
         super().__init__(workmodel, lgr)
         self._logger.info(f"Instantiated {type(self).__name__} concrete criterion")
 
-    def compute(self, object: list, p_src: Rank, _):
+    def compute(self, objects: list, r_src: Rank, _) -> float:
         """ A criterion enforcing strict conservation of local communications."""
 
         # Keep track source processor ID
-        p_src_id = p_src.get_id()
+        r_src_id = r_src.get_id()
 
-        # Retrieve object communications
-        # TODO: compute method should be updated like `TemperedCriterion`, object is a list, so iteration is needed
-        comm = object.get_communicator()
+        # Iterate over objects proposed for transfer
+        for o in objects:
+            # Retrieve object communications
+            comm = o.get_communicator()
 
-        # Iterate over sent messages
-        if not isinstance(comm, ObjectCommunicator):
-            self.__logger.warning(f"Object {object.get_id()} has no communicator")
-            return 0.
+            # Ignore object if it does not have a communicator
+            if not isinstance(comm, ObjectCommunicator):
+                continue
 
-        # Iterate over sent messages
-        for i in comm.get_sent().items():
-            if p_src_id == i[0].get_rank_id():
-                # Bail out as soon as locality is broken by transfer
-                return -1.
+            # Iterate over sent messages
+            for i in comm.get_sent().items():
+                if r_src_id == i[0].get_rank_id():
+                    # Bail out as soon as locality is broken by transfer
+                    return -1.
 
-        # Iterate over received messages
-        for i in comm.get_received().items():
-            if p_src_id == i[0].get_rank_id():
-                # Bail out as soon as locality is broken by transfer
-                return -1.
+            # Iterate over received messages
+            for i in comm.get_received().items():
+                if r_src_id == i[0].get_rank_id():
+                    # Bail out as soon as locality is broken by transfer
+                    return -1.
 
-        # Criterion returns a positive value meaning acceptance
+        # Accept transfer if this point was reached as no locality was broken
         return 1.
