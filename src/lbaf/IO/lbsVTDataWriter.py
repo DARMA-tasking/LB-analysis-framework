8235a83f49e7122b6e67b9a9d9f1ab83dd517b65
Philippe P. Pébaÿ <philippe.pebay@ng-analytics.com>
2023-03-23 19:59:55 +0100

    #354: cleaned up ancient datasets

--- src/lbaf/IO/lbsVTDataWriter.py
+++ src/lbaf/IO/lbsVTDataWriter.py
@@ -38,7 +38,7 @@ class VTDataWriter:
         self.__output_dir = output_dir
 
     def write(self):
-        """ Write one JSON file per rank. """
+        """ Write one JSON file per rank."""
         sys.setrecursionlimit(25000)
         with Pool(context=get_context("fork")) as pool:
             results = pool.imap_unordered(self.json_writer, self.__phase.get_ranks())
@@ -50,34 +50,15 @@ class VTDataWriter:
         file_name = f"{self.__file_stem}.{rank.get_id()}.{self.__suffix}"
         if self.__output_dir is not None:
             file_name = os.path.join(self.__output_dir, file_name)
-        # Count number of unsaved objects for sanity
-        n_u = 0
-        temp_dict = {}
-        # Iterate over objects
-        for o in rank.get_objects():
-            # Write object to file and increment count
-            try:
-                # writer.writerow([o.get_rank_id(), o.get_id(), o.get_load()])
-                rank_id = o.get_rank_id()
-                obj_id = o.get_id()
-                obj_load = o.get_load()
-                if isinstance(temp_dict.get(rank_id, None), list):
-                    temp_dict[rank_id].append({
-                        "rank_id": rank_id,
-                        "obj_id": obj_id,
-                        "obj_load": obj_load})
-                else:
-                    temp_dict[rank_id] = list()
-                    temp_dict[rank_id].append({
-                        "rank_id": rank_id,
-                        "obj_id": obj_id,
-                        "obj_load": obj_load})
-            except:
-                n_u += 1
+
+        # Create list of objects descriptions
+        objects = [
+            {"obj_id": o.get_id(), "obj_load": o.get_load()}
+            for o in rank.get_objects()]
+        print("objects on rank", rank.get_id(), ":", objects)
 
         dict_to_dump = {}
-        dict_to_dump["phases"] = list()
-        for rank_id, others_list in temp_dict.items():
+        for rank_id, others_list in object_map.items():
             phase_dict = {"tasks": list(), "id": rank_id}
             for task in others_list:
                 task_dict = {
@@ -85,18 +66,12 @@ class VTDataWriter:
                     "resource": "cpu",
                     "object": task["obj_id"]}
                 phase_dict["tasks"].append(task_dict)
-            dict_to_dump["phases"].append(phase_dict)
-
+            dict_to_dump.setdefault("phases", []).append(phase_dict)
+        print(dict_to_dump)
         json_str = json.dumps(dict_to_dump, separators=(',', ':'))
         compressed_str = brotli.compress(string=json_str.encode("utf-8"), mode=brotli.MODE_TEXT)
 
+        # Write file and return its name
         with open(file_name, "wb") as compr_json_file:
             compr_json_file.write(compressed_str)
-
-        # Sanity check
-        if n_u:
-            self.__logger.error(f"{n_u} objects could not be written to JSON file {file_name}")
-        else:
-            self.__logger.info(f"Wrote {len(rank.get_objects())} objects to {file_name}")
-
         return file_name
