e60147d5e5e4c46cacead3187aaeb0800c284971
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2023-04-26 12:13:43 +0200

    #377: test invalid import in a unit test

--- tests/test_tmp_377_failure.py
+++ tests/test_tmp_377_failure.py
@@ -1,5 +1,10 @@
 import unittest
 
+import blabla
+
 class TestConfig(unittest.TestCase):
-    def test_force_fail(self):
-        self.assertEqual(False, True)
+    # def test_force_fail(self):
+    #     self.assertEqual(False, True)
+
+    def test_true(self):
+        self.assertEqual(True, True)
