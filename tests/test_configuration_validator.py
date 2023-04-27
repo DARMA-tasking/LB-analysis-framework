929b35b69be23f2ec6691517c1e2478c89091d62
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2023-04-27 17:41:13 +0200

    #376: remove unnecessary statements

--- tests/test_configuration_validator.py
+++ tests/test_configuration_validator.py
@@ -1,14 +1,8 @@
 import os
 import sys
-try:
-    project_path = f"{os.sep}".join(os.path.abspath(__file__).split(os.sep)[:-2])
-    sys.path.append(project_path)
-except Exception as e:
-    print(f"Can not add project path to system path! Exiting!\nERROR: {e}")
-    raise SystemExit(1)
 
-from schema import SchemaError, SchemaMissingKeyError, SchemaOnlyOneAllowedError, SchemaUnexpectedTypeError
 import unittest
+from schema import SchemaError, SchemaMissingKeyError, SchemaOnlyOneAllowedError
 import yaml
 
 from src.lbaf.IO.lbsConfigurationValidator import ConfigurationValidator
