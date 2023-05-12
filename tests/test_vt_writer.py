a15fe06ecae2633220efd7127ae79fe5d3c2837b
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2023-05-12 15:05:24 +0200

    #355: add schema validator and update configuration file

--- tests/test_vt_writer.py
+++ tests/test_vt_writer.py
@@ -1,29 +1,30 @@
 """Tests for the VT Writer"""
+from collections import OrderedDict
+import json
 import os
+import subprocess
 import sys
 import unittest
-import filecmp
-import shutil
+
 import brotli
-import json
-import subprocess
 import yaml
 
-from lbaf.Utils.common import current_dir, project_dir, abspath_from, project_dir
-from lbaf.Utils.logger import logger
-from lbaf.IO.lbsVTDataReader import LoadReader
-from lbaf.IO.lbsVTDataWriter import VTDataWriter
-from lbaf.Model.lbsPhase import Phase
-from collections import OrderedDict 
+from lbaf.Utils.common import current_dir, project_dir, abspath_from
 
-# pylint:disable=C0115,C0116,W0212(protected-access)
 
-# TODO: Test VT WRITER
+from src.lbaf.Applications.schema_validator_loader import check_and_get_schema_validator
+check_and_get_schema_validator()
+
+from src.lbaf.imported.JSON_data_files_validator import SchemaValidator
+
+
 class TestVTDataWriter(unittest.TestCase):
-    ouput_dir: str
     """Test class for VTDataWriter"""
+
+    output_dir: str
+
     def setUp(self):
-        self.output_dir = current_dir() + '/output/vt_writer'
+        self.output_dir = current_dir() + '/output/vt_writer_null_test'
         if not os.path.exists(self.output_dir):
             os.makedirs(self.output_dir)
 
@@ -31,105 +32,8 @@ class TestVTDataWriter(unittest.TestCase):
         # shutil.rmtree(self.output_dir)
         return
 
-    # def test_run_lbaf(self):
-    #     """Test that LBAF runs correctly with demo configuration file at config/conf.yaml"""
-    #     lbaf_path = os.path.join(project_dir(), 'src', 'lbaf', 'Applications', 'LBAF_app.py')
-    #     lbaf_config_file = os.path.join(project_dir(), 'config', 'conf.yaml')
-    #     lbaf_run = subprocess.run(
-    #         [
-    #             sys.executable,
-    #             lbaf_path,
-    #             '-c',
-    #             lbaf_config_file
-    #         ],
-    #         check=True,
-    #         stdout=subprocess.PIPE,
-    #         stderr=subprocess.STDOUT
-    #     )
-    #     self.assertEqual(0, lbaf_run.returncode)
-
-    # def test_vt_writer_run(self):
-    #     """Test that vt files are exactly the identical to the input data when using the VT writer
-    #     1. read in the VT data in  data_stem: ../data/challenging_toy_fewer_tasks/toy;
-    #     2. execute LBAF using, e.g., 0 iterations of the phase stepper algorithm;
-    #     3. write the VT output data files and verify those exactly identical to the input data.
-    #     """
-        
-    #     lbaf_path = os.path.join(project_dir(), 'src', 'lbaf', 'Applications', 'LBAF_app.py')
-    #     lbaf_config_dir = os.path.join(current_dir(), 'config')
-    #     lbaf_config_file = os.path.join(lbaf_config_dir, 'conf_correct_vt_writer.yml')
-    #     lbaf_config = {}
-    #     with open(lbaf_config_file, "rt", encoding="utf-8") as lbaf_config_io:
-    #         lbaf_config = yaml.safe_load(lbaf_config_io.read())
-    #     lbaf_output_dir = abspath_from(lbaf_config.get('output_dir'), lbaf_config_dir)
-    #     lbaf_run = subprocess.run(
-    #         [
-    #             sys.executable,
-    #             lbaf_path,
-    #             '-c',
-    #             lbaf_config_file
-    #         ],
-    #         check=True,
-    #         stdout=subprocess.PIPE,
-    #         stderr=subprocess.STDOUT
-    #     )
-
-    #     # check that lbaf has run correctly
-    #     # self.assertEqual(0, lbaf_run.returncode)
-
-    #     # check that expected output files exists and content is same than input files
-    #     # get some info from the config file
-    #     data_stem = lbaf_config.get('from_data').get('data_stem')
-    #     lbaf_input_dir = f"{os.sep}".join(data_stem.split(os.sep)[:-1])
-    #     lbaf_input_dir = abspath_from(lbaf_input_dir, lbaf_config_dir)
-    #     input_file_prefix = data_stem.split(os.sep)[-1]
-    #     n_ranks = len([name for name in os.listdir(lbaf_input_dir)])
-    #     output_file_prefix = lbaf_config.get('output_file_stem')
-    #     for i in range(0, n_ranks):
-    #         input_file_name = f"{input_file_prefix}.{i}.json"
-    #         input_file = os.path.join(lbaf_input_dir, input_file_name)
-    #         output_file_name = f"{output_file_prefix}.{i}.vom"
-    #         output_file = os.path.join(lbaf_output_dir, output_file_name)
-
-    #         # check output file exists
-    #         self.assertTrue(
-    #             os.path.isfile(output_file),
-    #             f'File {output_file_name} not generated at {lbaf_output_dir}')
-
-    #         # check output file contains the same content than the input file
-    #         input_file_content = ""
-    #         with open(input_file, "rt", encoding="utf-8") as input_file_io:
-    #             input_file_content = input_file_io.read()
-    #         output_file_content = ""
-    #         with open(output_file, 'rb') as output_file_io:
-    #             output_file_content = output_file_io.read()
-    #             output_file_content = brotli.decompress(output_file_content)
-    #             output_file_content = output_file_content.decode('utf-8')
-
-    #         print("--------------------")
-    #         print(input_file_content)
-    #         print("--------------------")
-    #         print(output_file_content)
-    #         print("--------------------")
-
-    #         input_file_data = json.loads(input_file_content)
-    #         output_file_data = json.loads(output_file_content)
-    #         print("--------------------")
-    #         print(input_file_data)
-    #         print("--------------------")
-    #         print(output_file_data)
-    #         print("--------------------")
-
-    #         self.assertEqual(input_file_content, output_file_content)
-
-    # def test_vt_writer_run(self):
-    #     # reader reads a file
-    #     # writer writes a file
-    #     # compare
-
-    def _run(self, config_file) -> subprocess.CompletedProcess:
-        """Runs LBAF as a subprocess
-        """
+    def _run_lbaf(self, config_file) -> subprocess.CompletedProcess:
+        """Run lbaf as a subprocess"""
 
         lbaf_path = os.path.join(project_dir(), 'src', 'lbaf', 'Applications', 'LBAF_app.py')
         proc = subprocess.run(
@@ -146,9 +50,10 @@ class TestVTDataWriter(unittest.TestCase):
         return proc
 
     def test_vt_writer_null_test_valid_output(self):
-        # run LBAF with a null test
+        """run LBAF with a null test"""
+
         config_file = os.path.join(os.path.dirname(__file__), 'config', 'conf_vt_writer_null_test.yml')
-        proc = self._run(config_file)
+        proc = self._run_lbaf(config_file)
         self.assertEqual(0, proc.returncode)
 
         # check output
@@ -168,23 +73,29 @@ class TestVTDataWriter(unittest.TestCase):
             output_file_name = f"{output_file_prefix}.{i}.json"
             output_file = os.path.join(output_dir, output_file_name)
 
-            # check output file exists
+            # validate file has been written
             self.assertTrue(
                 os.path.isfile(output_file),
                 f'File {output_file} not generated at {output_dir}')
 
-            # check output file contains the same content than the input file
+            # retrieve generated content
             input_file_content = ""
             with open(input_file, "rt", encoding="utf-8") as input_file_io:
                 input_file_content = input_file_io.read()
-            output_file_content = ""
-            with open(output_file, 'rb') as output_file_io:
-                output_file_content = output_file_io.read()
-                output_file_content = brotli.decompress(output_file_content)
-                output_file_content = output_file_content.decode('utf-8')
+                input_dict = json.loads(input_file_content, object_pairs_hook=OrderedDict)
+            with open(output_file, "rb") as output_file_io:
+                compr_bytes = output_file_io.read()
+                try:
+                    output_file_content = brotli.decompress(compr_bytes)
+                    output_dict = json.loads(output_file_content.decode("utf-8"), object_pairs_hook=OrderedDict)
+                except brotli.error:
+                    output_dict = json.loads(compr_bytes.decode("utf-8"), object_pairs_hook=OrderedDict)
 
-            input_dict = json.loads(input_file_content, object_pairs_hook=OrderedDict)
-            output_dict = json.loads(output_file_content, object_pairs_hook=OrderedDict)
+            # validate output against the JSON schema validator
+            self.assertTrue(
+                SchemaValidator(schema_type="LBDatafile").is_valid(schema_to_validate=output_dict),
+                f"Invalid JSON schema in {output_file_name}"
+            )
 
             print(f"-------------------------------{input_file_name}-----------------------------------")
             print(json.dumps(input_dict, indent=4, sort_keys = True))
