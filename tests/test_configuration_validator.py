4d2b43aff4f818490431cc9cafaa0db7d0ab791f
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2023-06-08 19:09:48 +0200

    #396: fix bad import

--- tests/test_configuration_validator.py
+++ tests/test_configuration_validator.py
@@ -6,7 +6,7 @@ import yaml
 
 from lbaf.Utils.path import abspath
 from lbaf.IO.lbsConfigurationValidator import ConfigurationValidator
-from lbaf.Utils.logging import get_logger
+from lbaf.Utils.logger import get_logger
 
 
 class TestConfig(unittest.TestCase):
