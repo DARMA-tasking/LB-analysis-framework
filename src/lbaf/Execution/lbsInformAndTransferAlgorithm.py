fdf14e586d717636ce7ff46a8e881c753aaf4fd4
Philippe P. Pébaÿ <philippe.pebay@ng-analytics.com>
2023-06-11 08:44:31 -0400

    #323: completed refactoring of information stage

--- src/lbaf/Execution/lbsInformAndTransferAlgorithm.py
+++ src/lbaf/Execution/lbsInformAndTransferAlgorithm.py
@@ -79,20 +79,23 @@ class InformAndTransferAlgorithm(AlgorithmBase):
         # Initialize empty dictionary of known peers
         self.__known_peers = {}
 
-    def __forward_message(self, i: int, r: Rank, loads: set, f:int):
-        """Forward information message to sample of selected peers."""
+    def __process_message(self, r_rcv: Rank, m: Message):
+        """Process message received by rank."""
+        self.__known_peers[r_rcv].update(m.get_support())
+
+    def __forward_message(self, i: int, r_snd: Rank, loads: set, f:int):
+        """Forward information message to rank peers sampled from known ones."""
         # Create load message tagged at given information round
-        msg = Message(i, {
-            "loads": r.get_known_loads()})
+        msg = Message(i, self.__known_peers[r_snd])
 
         # Compute complement of set of known peers
-        complement = set(r.get_known_loads()).difference([r])
+        complement = self.__known_peers[r_snd].difference({r_snd})
 
         # Forward message to pseudo-random sample of ranks
         return random.sample(
             list(complement), min(f, len(complement))), msg
 
-    def __information_stage(self):
+    def __execute_information_stage(self):
         """Execute information stage."""
 
         # Build set of all ranks in the phase
@@ -100,6 +103,7 @@ class InformAndTransferAlgorithm(AlgorithmBase):
 
         # Initialize information messages and known peers
         messages = {}
+        n_r = len(rank_set)
         for r_snd in rank_set:
             # Make rank aware of itself
             self.__known_peers[r_snd] = {r_snd}
@@ -109,25 +113,21 @@ class InformAndTransferAlgorithm(AlgorithmBase):
 
             # Broadcast message to random sample of ranks excluding self
             for r_rcv in random.sample(
-                list(rank_set.difference([r_snd])),
-                min(self.__fanout, len(rank_set) - 1)):
+                list(rank_set.difference({r_snd})), min(self.__fanout, n_r - 1)):
                 messages.setdefault(r_rcv, []).append(msg)
 
         # Sanity check prior to forwarding iterations
-        if (n_m := sum([len(m) for m in messages.values()])) != (n_c := len(rank_set) * self.__fanout):
+        if (n_m := sum([len(m) for m in messages.values()])) != (n_c := n_r * self.__fanout):
             self._logger.error(
                 f"Incorrect number of initial messages: {n_m} <> {n_c}")
         self._logger.info(
             f"Sent {n_m} initial information messages with fanout={self.__fanout}")
 
-        for k, v in messages.items():
-            print(k, v)
-
-        # Process all messages of first round
+        # Process all received initial messages
         for r_rcv, m_rcv in messages.items():
             for m in m_rcv:
                 # Process message by recipient
-                self.__known_peers[r_rcv].update(m.get_support())
+                self.__process_message(r_rcv, m)
 
         # Perform sanity check on first round of information aggregation
         n_k = 0
@@ -137,9 +137,9 @@ class InformAndTransferAlgorithm(AlgorithmBase):
             n_k += len(k_p)
             self._logger.debug(
                 f"Peers known to rank {r.get_id()}: {[r_k.get_id() for r_k in k_p]}")
-        if n_k != (n_c := n_c + len(rank_set)):
+        if n_k != (n_c := n_c + n_r):
             self._logger.error(
-                f"Incorrect total number of aggregated initial messages: {n_k} <> {n_c}")
+                f"Incorrect total number of aggregated initial known peers: {n_k} <> {n_c}")
 
         # Forward messages for as long as necessary and requested
         for i in range(1, self.__n_rounds):
@@ -163,8 +163,12 @@ class InformAndTransferAlgorithm(AlgorithmBase):
             # Report on gossiping status when requested
             for p in rank_set:
                 self._logger.debug(
-                    f"information known to rank {p.get_id()}: "
-                    f"{[p_u.get_id() for p_u in p.get_known_loads()]}")
+                    f"Peers known to rank {r.get_id()}: {[r_k.get_id() for r_k in k_p]}")
+
+        # Report on final know information ratio
+        n_k = sum([len(k_p) for k_p in self.__known_peers.values() if k_p]) / n_r
+        self._logger.info(
+            f"Average number of peers known to ranks: {n_k} ({100 * n_k / n_r:.2f}% of {n_r})")
 
     def execute(self, p_id: int, phases: list, distributions: dict, statistics: dict, a_min_max):
         """ Execute 2-phase information+transfer algorithm on Phase with index p_id."""
@@ -182,7 +186,7 @@ class InformAndTransferAlgorithm(AlgorithmBase):
             self._logger.info(f"Starting iteration {i + 1} with total work of {total_work}")
 
             # Start with information stage
-            self.__information_stage()
+            self.__execute_information_stage()
 
             # Then execute transfer stage
             n_ignored, n_transfers, n_rejects = self.__transfer_strategy.execute(
