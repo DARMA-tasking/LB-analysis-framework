67270469fa0abbcbf60ef5933291f3bf2dec91ef
Caleb Schilly <cwschilly@gmail.com>
2023-07-25 13:25:39 -0400

    #337: formatting changes

--- tests/unit/test_lbs_statistics.py
+++ tests/unit/test_lbs_statistics.py
@@ -3,59 +3,14 @@ import math
 import random as rnd
 from numpy import random
 import unittest
-from unittest.mock import patch
 
 import lbaf.IO.lbsStatistics as lbsStatistics
 from lbaf.IO.lbsStatistics import Statistics
 
-def test_function(x):
-    return x
 
 class TestConfig(unittest.TestCase):
-    # def setUp(self):
-    #     self.logger = logging.getLogger()
-
-    #     # Initialize statistics (currently nonsense)
-    #     n=1
-    #     mini=0.0
-    #     mean=2.5
-    #     maxi=5.0
-    #     var=3.0
-    #     g1=1.0
-    #     g2=1.0
-
-    #     # Manually store primary and derived statistics
-    #     self.expected_primary_statistics = {
-    #         "cardinality": n,
-    #         "minimum": mini,
-    #         "average": mean,
-    #         "maximum": maxi,
-    #         "variance": var,
-    #         "skewness": g1,
-    #         "kurtosis": g2}
-    #     self.expected_derived_statistics = {
-    #         "sum": n * mean,
-    #         "imbalance":  maxi / mean - 1.0,
-    #         "standard deviation": math.sqrt(var),
-    #         "kurtosis excess": g2 - 3.0}
-
-    #     # Create instance of Statistics class
-    #     self.statistics = Statistics(
-    #         n,
-    #         mini,
-    #         mean,
-    #         maxi,
-    #         var,
-    #         g1,
-    #         g2)
-
-    # def test_lbs_stats_initialization(self):
-    #     self.assertEqual(self.statistics.get_minimum(), self.mini)
-    #     self.assertEqual(self.statistics.get_maximum(), self.maxi)
-    #     self.assertEqual(self.statistics.get_average(), self.mean)
-    #     self.assertAlmostEqual(self.statistics.get_variance(), self.var)
-    #     self.assertAlmostEqual(self.statistics.get_skewness(), self.g1)
-    #     self.assertAlmostEqual(self.statistics.get_kurtosis(), self.g2)
+    def setUp(self):
+        self.logger = logging.getLogger()
 
     def test_lbs_stats_compute_function_statistics(self):
 
@@ -81,7 +36,7 @@ class TestConfig(unittest.TestCase):
         expected_average = sum(sample_pop) / len(sample_pop)
         expected_variance = sum((x - expected_average) ** 2 for x in sample_pop) / len(sample_pop)
         expected_skewness = 0
-        expected_kurtosis = -1.2
+        expected_kurtosis = 1.7
 
         # Expected derived statistics based on the input population and computed primary statistics
         expected_sum = sum(sample_pop)
@@ -103,9 +58,8 @@ class TestConfig(unittest.TestCase):
         self.assertAlmostEqual(result.get_standard_deviation(), expected_standard_deviation)
         self.assertAlmostEqual(result.get_kurtosis_excess(), expected_kurtosis_excess)
 
-    # def test_lbs_stats_compute_all_reachable_arrangements(self):
-
-    # def test_lbs_stats_recursively_compute_transitions(self):
+def test_function(x):
+    return x
 
 if __name__ == "__main__":
     unittest.main()
