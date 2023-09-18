6b30783590f8d7996ef5ae3668f29309098f9ed6
Caleb Schilly <132086024+cwschilly@users.noreply.github.com>
2023-09-18 07:29:08 -0700

    #434: Make cluster swap relative threshold a configurable parameter (#436)
    * #434: add optional cluster_swap_rtol parameter
    
    * #434: simplify code
    
    * #434: test new parameter in CI

--- src/lbaf/Execution/lbsClusteringTransferStrategy.py
+++ src/lbaf/Execution/lbsClusteringTransferStrategy.py
@@ -22,6 +22,9 @@ class ClusteringTransferStrategy(TransferStrategyBase):
         # Call superclass init
         super(ClusteringTransferStrategy, self).__init__(criterion, parameters, lgr)
 
+        # Initialize cluster swap relative threshold
+        self._cluster_swap_rtol = parameters.get("cluster_swap_rtol",0.05)
+
     def __cluster_objects(self, rank):
         """Cluster migratiable objects by shared block ID when available."""
         # Iterate over all migratable objects on rank
@@ -40,8 +43,9 @@ class ClusteringTransferStrategy(TransferStrategyBase):
             k: clusters[k]
             for k in random.sample(clusters.keys(), len(clusters))}
 
-    def __find_suitable_subclusters(self, clusters, rank_load, r_tol=0.05):
+    def __find_suitable_subclusters(self, clusters, rank_load):
         """Find suitable sub-clusters to bring rank closest and above average load."""
+
         # Bail out early if no clusters are available
         if not clusters:
             self._logger.info("No migratable clusters on rank")
@@ -64,7 +68,7 @@ class ClusteringTransferStrategy(TransferStrategyBase):
                     for p in nr.binomial(n_o, 0.5, n_o))):
                 # Reject subclusters overshooting within relative tolerance
                 reach_load = rank_load - sum([o.get_load() for o in c])
-                if reach_load < (1.0 - r_tol) * self._average_load:
+                if reach_load < (1.0 - self._cluster_swap_rtol) * self._average_load:
                     continue
 
                 # Retain suitable subclusters with their respective distance and cluster
@@ -114,7 +118,8 @@ class ClusteringTransferStrategy(TransferStrategyBase):
                         if c_try > 0.0:
                             # Compute source cluster size only when necessary
                             sz_src = sum([o.get_load() for o in o_src])
-                            if  c_try > 0.05 * sz_src:
+                            self._logger.warning(f"Cluster Tol: {self._cluster_swap_rtol}")
+                            if  c_try > self._cluster_swap_rtol * sz_src:
                                 # Perform swap
                                 self._logger.info(
                                     f"Swapping cluster {k_src} of size {sz_src} with cluster {k_try} on {r_try.get_id()}")
