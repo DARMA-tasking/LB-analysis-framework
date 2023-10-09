5b95db162eacf0f09aa26996b4a63708059e7571
Caleb Schilly <132086024+cwschilly@users.noreply.github.com>
2023-10-09 12:35:23 -0400

    #431: Add a target imbalance threshold to yaml (#455)
    * #431: create optional target_imbalance parameter
    
    * #431: add clarifying comment

--- src/lbaf/Execution/lbsInformAndTransferAlgorithm.py
+++ src/lbaf/Execution/lbsInformAndTransferAlgorithm.py
@@ -7,7 +7,6 @@ from .lbsCriterionBase import CriterionBase
 from .lbsTransferStrategyBase import TransferStrategyBase
 from ..Model.lbsRank import Rank
 from ..Model.lbsMessage import Message
-from ..IO.lbsStatistics import print_function_statistics, min_Hamming_distance
 
 
 class InformAndTransferAlgorithm(AlgorithmBase):
@@ -70,6 +69,9 @@ class InformAndTransferAlgorithm(AlgorithmBase):
 
         # No information about peers is known initially
         self.__known_peers = {}
+
+        # Optional parameter to conclude the iteration process iff imbalance is below the target threshold
+        self.__target_imbalance = parameters.get("target_imbalance", 0.0)
 
     def get_known_peers(self):
         """Return all known peers."""
@@ -207,11 +209,11 @@ class InformAndTransferAlgorithm(AlgorithmBase):
             self._logger.info(f"Iteration complete ({n_ignored} skipped ranks)")
 
             # Compute and report iteration work statistics
-            print_function_statistics(
-                self._rebalanced_phase.get_ranks(),
-                lambda x: self._work_model.compute(x),  # pylint:disable=W0108:unnecessary-lambda
-                f"iteration {i + 1} rank work",
-                self._logger)
+            stats = print_function_statistics(
+                        self._rebalanced_phase.get_ranks(),
+                        lambda x: self._work_model.compute(x),  # pylint:disable=W0108:unnecessary-lambda
+                        f"iteration {i + 1} rank work",
+                        self._logger)
 
             # Update run distributions and statistics
             self._update_distributions_and_statistics(distributions, statistics)
@@ -230,6 +232,11 @@ class InformAndTransferAlgorithm(AlgorithmBase):
                 self._logger.info(
                     f"Iteration {i + 1} minimum Hamming distance to optimal arrangements: {hd_min}")
                 statistics["minimum Hamming distance to optimum"].append(hd_min)
+
+            # Check if the current imbalance is within the target_imbalance range
+            if stats.statistics["imbalance"] <= self.__target_imbalance:
+                self._logger.info(f"Reached target imbalance of {self.__target_imbalance} after {i + 1} iterations.")
+                break
 
         # Report final mapping in debug mode
         self._report_final_mapping(self._logger)
