8202f1160fd9453fc09e2a3fec7c534934081101
Philippe P. Pébaÿ <philippe.pebay@ng-analytics.com>
2023-10-09 05:48:37 -0400

    #446: retaining one debug statement as information message

--- src/lbaf/Execution/lbsAlgorithmBase.py
+++ src/lbaf/Execution/lbsAlgorithmBase.py
@@ -211,7 +211,8 @@ class AlgorithmBase:
             self._logger.error("Algorithm execution requires a dictionary of phases")
             raise SystemExit(1)
 
-        # Create a rebalanced phase to preserve phase to be rebalanced
+        # Create a new phase to preserve phase to be rebalanced
+        self._logger.info(f"Creating new phase {p_id} for rebalancing")
         self._rebalanced_phase = Phase(self._logger, p_id)
 
         # Try to copy ranks from phase to be rebalanced to processd one
