72414a4e385c8440e3cfee8b0f1a6e68b5d8a382
Caleb Schilly <cwschilly@gmail.com>
2023-12-18 12:38:28 -0500

    #484: fix bad indentation

--- src/lbaf/Execution/lbsTransferStrategyBase.py
+++ src/lbaf/Execution/lbsTransferStrategyBase.py
@@ -66,7 +66,7 @@ class TransferStrategyBase:
         for r_src in ranks:
             # Ignore ranks without migratable objects
             if not r_src.get_migratable_objects():
-               continue
+                continue
 
             # Retrieve potential targets
             targets = known_peers.get(r_src, set()).difference({r_src})
