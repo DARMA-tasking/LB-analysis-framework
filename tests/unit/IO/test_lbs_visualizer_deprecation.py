423836749c4f6dc923eafef0b29d81b171d79e27
Caleb Schilly <cwschilly@gmail.com>
2024-05-20 10:11:16 -0400

    #507: add config file test for lbaf viz deprecation

--- tests/unit/IO/test_lbs_visualizer_deprecation.py
+++ tests/unit/IO/test_lbs_visualizer_deprecation.py
@@ -1,14 +1,13 @@
 import os
 import logging
 import unittest
+import subprocess
 from src.lbaf.IO.lbsVisualizer import Visualizer
 
 class TestVizDeprecation(unittest.TestCase):
     """Test for lbsVisualizer's deprecation."""
 
     def test_lbs_visualizer_deprecation(self):
-
-        # Instantiate dummy Visualizer to ensure Deprecation Error is thrown
         try:
             visualizer = Visualizer(
                 logger=logging.getLogger(),
@@ -18,6 +17,14 @@ class TestVizDeprecation(unittest.TestCase):
                 grid_size=[0,0,1,1])
         except DeprecationWarning as e:
             assert str(e) == "LBAF's Visualizer has been deprecated and will be removed in a future release. Visualizations should be generated with DARMA/vt-tv."
+
+    def test_lbs_visualizer_config(self):
+        config_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), "config", "conf_wrong_visualization.yml")
+        pipes = subprocess.Popen(["python", "src/lbaf", "-c", config_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        std_err = pipes.communicate()[1].decode("utf-8")
+
+        assert pipes.returncode != 0
+        assert "DeprecationWarning: LBAF\'s Visualizer has been deprecated and will be removed in a future release. Visualizations should be generated with DARMA/vt-tv." in std_err
 
 if __name__ == "__main__":
     unittest.main()
