9884cb190963271767c89208cd5ef604e0c23b85
Caleb Schilly <cwschilly@gmail.com>
2024-05-21 16:31:15 -0400

    #507: remove config file test; clean up viz deprecation

--- tests/unit/IO/test_lbs_visualizer_deprecation.py
+++ tests/unit/IO/test_lbs_visualizer_deprecation.py
@@ -18,13 +18,5 @@ class TestVizDeprecation(unittest.TestCase):
         except DeprecationWarning as e:
             assert str(e) == "LBAF's Visualizer has been deprecated and will be removed in a future release. Visualizations should be generated with DARMA/vt-tv."
 
-    def test_lbs_visualizer_config(self):
-        config_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), "config", "conf_wrong_visualization.yml")
-        pipes = subprocess.Popen(["python", "src/lbaf", "-c", config_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        std_err = pipes.communicate()[1].decode("utf-8")
-
-        assert pipes.returncode != 0
-        assert "DeprecationWarning: LBAF\'s Visualizer has been deprecated and will be removed in a future release. Visualizations should be generated with DARMA/vt-tv." in std_err
-
 if __name__ == "__main__":
     unittest.main()
