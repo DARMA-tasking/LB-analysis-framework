304736abcbe296929b8e4b2c1fb43badf19d56d1
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-06-27 12:53:23 +0200

    #506: add unit tests for data files maker

---
+++ tests/unit/Utils/test_json_data_files_maker.py
@@ -0,0 +1,55 @@
+import os
+import unittest
+from unittest.mock import patch
+import argparse
+from typing import Callable, Any
+
+from src.lbaf.Utils.lbsJSONDataFilesMaker import JSONDataFilesMaker
+
+class TestVTDataFilesMaker(unittest.TestCase):
+
+    test_dir: str = os.path.dirname(os.path.dirname(__file__))
+    config_dir: str = os.path.join(test_dir, "config", "phases")
+    output_dir: str = os.path.join(test_dir, "output", "JSON_data_files_maker")
+
+    argparse_args: Callable[Any, argparse.Namespace] = lambda **kwargs: argparse.Namespace(
+        spec_file=kwargs.get("spec_file"),
+        data_stem=kwargs.get("data_stem"),
+        interactive=kwargs.get("interactive", False),
+        multiple_sharing=kwargs.get("multiple_sharing", False),
+        output_config_file=kwargs.get("output_config_file", None),
+        compressed=False
+    )
+
+    @patch('argparse.ArgumentParser.parse_args', return_value=argparse_args(
+        spec_file=os.path.join(config_dir, "phase_spec_01_wrong.yaml"),
+        data_stem=os.path.join(output_dir, "dataset01")
+    ))
+    def test_make_data_files_from_spec_wrong_01(self, namespace: argparse.Namespace):
+        """Test that invalid phase configuration 01 generates no dataset and fire error"""
+
+        with self.assertRaises(SystemExit):
+            JSONDataFilesMaker().run()
+
+    @patch('argparse.ArgumentParser.parse_args', return_value=argparse_args(
+        spec_file=os.path.join(config_dir, "phase_spec_02_wrong.yaml"),
+        data_stem=os.path.join(output_dir, "dataset02")
+    ))
+    def test_make_data_files_from_spec_wrong_02(self, namespace: argparse.Namespace):
+        """Test that invalid phase configuration 02 generates no dataset and fire error"""
+
+        with self.assertRaises(SystemExit):
+            JSONDataFilesMaker().run()
+
+    @patch('argparse.ArgumentParser.parse_args', return_value=argparse_args(
+        spec_file=os.path.join(config_dir, "phase_spec_03_valid.yaml"),
+        data_stem=os.path.join(output_dir, "dataset03")
+    ))
+    def test_make_data_files_from_spec_valid_03(self, namespace: argparse.Namespace):
+        """Test that valid phase configuration correctly generates a dataset""" 
+
+        print(namespace)
+        JSONDataFilesMaker().run()
+
+if __name__ == "__main__":
+    unittest.main()
