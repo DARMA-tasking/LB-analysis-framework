6594331507c85824e5bbd8e6dd210da756e7f38f
Caleb Schilly <132086024+cwschilly@users.noreply.github.com>
2024-08-06 09:47:43 -0400

    #523:  Fix CI after JSONValidator changes (#524)
    * #523: update JSON validator test with new args
    
    * #523: add tests for new args
    
    * #523: use different data for validating comm links

--- tests/unit/imported/test_JSON_data_files_validator.py
+++ tests/unit/imported/test_JSON_data_files_validator.py
@@ -19,6 +19,7 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         self.test_dir = os.path.dirname(os.path.dirname(__file__))
         self.data_dir = os.path.join(self.test_dir, "data")
         self.file_path = os.path.join(self.data_dir, "JSON_data_file_validator", "data.0.json")
+        self.comm_links_file_path = os.path.join(self.data_dir, "JSON_data_file_validator_comm_links", "data.0.json")
         self.wrong_file_path = os.path.join(self.data_dir, "JSON_data_file_validator", "data.0.jso")
         self.wrong_file_schema = os.path.join(self.data_dir, "JSON_data_file_validator_wrong", "data.0.json")
         self.stats_file_001 = os.path.join(self.data_dir, "JSON_data_file_validator_stats", "vt_lb_statistics_001.json")
@@ -40,7 +41,9 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=self.file_path,
                                                                              dir_path=None,
                                                                              file_prefix=None,
-                                                                             file_suffix=None)
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         JSONDataFilesValidator().main()
 
     def test_json_data_files_validator_dir(self):
@@ -48,7 +51,9 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=None,
                                                                              dir_path=self.dir_path,
                                                                              file_prefix=None,
-                                                                             file_suffix=None)
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         JSONDataFilesValidator().main()
 
     def test_json_data_files_validator_dir_compressed(self):
@@ -56,7 +61,9 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=None,
                                                                              dir_path=self.dir_path_compressed,
                                                                              file_prefix=None,
-                                                                             file_suffix=None)
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         JSONDataFilesValidator().main()
 
     def test_json_data_files_validator_file_stats_001(self):
@@ -64,7 +71,9 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=self.stats_file_001,
                                                                              dir_path=None,
                                                                              file_prefix=None,
-                                                                             file_suffix=None)
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         JSONDataFilesValidator().main()
 
     def test_json_data_files_validator_file_stats_002(self):
@@ -72,7 +81,9 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=self.stats_file_002,
                                                                              dir_path=None,
                                                                              file_prefix=None,
-                                                                             file_suffix=None)
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         JSONDataFilesValidator().main()
 
     def test_json_data_files_validator_file_stats_003(self):
@@ -80,7 +91,9 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=self.stats_file_003,
                                                                              dir_path=None,
                                                                              file_prefix=None,
-                                                                             file_suffix=None)
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         JSONDataFilesValidator().main()
 
     def test_json_data_files_validator_file_stats_004(self):
@@ -88,13 +101,17 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=self.stats_file_004,
                                                                              dir_path=None,
                                                                              file_prefix=None,
-                                                                             file_suffix=None)
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         JSONDataFilesValidator().main()
 
     def test_json_data_files_validator_file_stats_no_schema_type(self):
         argparse.ArgumentParser.parse_args = Mock()
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(
-            file_path=self.vt_lb_statistics_no_schema_type, dir_path=None, file_prefix=None, file_suffix=None)
+            file_path=self.vt_lb_statistics_no_schema_type, dir_path=None, file_prefix=None, file_suffix=None,
+            validate_comm_links=False,
+            debug=False)
         JSONDataFilesValidator().main()
 
     def test_json_data_files_validator_prefix(self):
@@ -102,7 +119,9 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=None,
                                                                              dir_path=self.dir_path,
                                                                              file_prefix="data",
-                                                                             file_suffix=None)
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         JSONDataFilesValidator().main()
 
     def test_json_data_files_validator_suffix(self):
@@ -110,7 +129,9 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=None,
                                                                              dir_path=self.dir_path,
                                                                              file_prefix=None,
-                                                                             file_suffix="json")
+                                                                             file_suffix="json",
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         JSONDataFilesValidator().main()
 
     def test_json_data_files_validator_prefix_suffix(self):
@@ -118,7 +139,9 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=None,
                                                                              dir_path=self.dir_path,
                                                                              file_prefix="data",
-                                                                             file_suffix="json")
+                                                                             file_suffix="json",
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         JSONDataFilesValidator().main()
 
     def test_json_data_files_validator_file_not_found(self):
@@ -126,7 +149,9 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=self.wrong_file_path,
                                                                              dir_path=None,
                                                                              file_prefix=None,
-                                                                             file_suffix=None)
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         with self.assertRaises(FileNotFoundError) as err:
             JSONDataFilesValidator().main()
         self.assertEqual(err.exception.args[0], f"File: {self.wrong_file_path} NOT found")
@@ -136,7 +161,9 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=None,
                                                                              dir_path=self.wrong_dir_path,
                                                                              file_prefix=None,
-                                                                             file_suffix=None)
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         with self.assertRaises(FileNotFoundError) as err:
             JSONDataFilesValidator().main()
         self.assertEqual(err.exception.args[0], f"Directory: {self.wrong_dir_path} does NOT exist")
@@ -146,7 +173,9 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=None,
                                                                              dir_path=None,
                                                                              file_prefix=None,
-                                                                             file_suffix=None)
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         with self.assertRaises(Exception) as err:
             JSONDataFilesValidator().main()
         self.assertEqual(err.exception.args[0], "FILE path or DIRECTORY path has to be given")
@@ -156,13 +185,34 @@ class TestJSONDataFilesValidator(unittest.TestCase):
         argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=self.wrong_file_schema,
                                                                              dir_path=None,
                                                                              file_prefix=None,
-                                                                             file_suffix=None)
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=False,
+                                                                             debug=False)
         with self.assertRaises(SchemaError) as err:
             JSONDataFilesValidator().main()
         with open(os.path.join(self.data_dir, "JSON_data_file_validator_wrong", "schema_error.txt"), "rt") as se:
             err_msg = se.read()
         self.assertEqual(err.exception.args[0], err_msg)
 
+    def test_json_data_files_validate_comm_links(self):
+        argparse.ArgumentParser.parse_args = Mock()
+        argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=self.comm_links_file_path,
+                                                                             dir_path=None,
+                                                                             file_prefix=None,
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=True,
+                                                                             debug=False)
+        JSONDataFilesValidator().main()
+
+    def test_json_data_files_debug(self):
+        argparse.ArgumentParser.parse_args = Mock()
+        argparse.ArgumentParser.parse_args.return_value = argparse.Namespace(file_path=self.file_path,
+                                                                             dir_path=None,
+                                                                             file_prefix=None,
+                                                                             file_suffix=None,
+                                                                             validate_comm_links=False,
+                                                                             debug=True)
+        JSONDataFilesValidator().main()
 
 if __name__ == "__main__":
     unittest.main()
