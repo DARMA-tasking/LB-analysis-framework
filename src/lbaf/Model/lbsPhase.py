af44ad1c0a44adb3856a8020a9393fc9b5fe461c
Caleb Schilly <132086024+cwschilly@users.noreply.github.com>
2024-08-14 15:27:23 -0400

    #525: replace all instances of shared_id with shared_block_id in LBAF (#526)

--- src/lbaf/Model/lbsPhase.py
+++ src/lbaf/Model/lbsPhase.py
@@ -459,9 +459,9 @@ class Phase:
                 ranks[rank_id].add_migratable_object(o)
 
         # Load shared blocks
-        for shared_id, shared_block_spec in spec["shared_blocks"].items():
+        for shared_block_id, shared_block_spec in spec["shared_blocks"].items():
             if not shared_block_spec["tasks"]:
-                self.__logger.warning(f"No tasks linked to shared block {shared_id} !")
+                self.__logger.warning(f"No tasks linked to shared block {shared_block_id} !")
 
             # Browse tasks
             for task_id in shared_block_spec["tasks"]:
@@ -469,23 +469,23 @@ class Phase:
 
                 # Find shared block and create if not already created in memory
                 b: Block = None
-                if not shared_id in shared_blocks:
-                    b = Block(b_id=shared_id, h_id=shared_block_spec["home_rank"], size=shared_block_spec["size"],
+                if not shared_block_id in shared_blocks:
+                    b = Block(b_id=shared_block_id, h_id=shared_block_spec["home_rank"], size=shared_block_spec["size"],
                               o_ids=shared_block_spec["tasks"])
                     # Index the shared block for next loops checks
-                    shared_blocks[shared_id] = b
+                    shared_blocks[shared_block_id] = b
                 else:
-                    b = shared_blocks[shared_id]
+                    b = shared_blocks[shared_block_id]
 
                 # Associate shared block with object
                 # Check: task must share 0 or 1 block
-                if not multiple_sharing and o.get_shared_block_id() is not None and o.get_shared_block_id() != shared_id:
+                if not multiple_sharing and o.get_shared_block_id() is not None and o.get_shared_block_id() != shared_block_id:
                     raise RuntimeError(
-                        f"Task {o.get_id()} already shared block {o.get_shared_block_id()} and cannot share additional block {shared_id}. Only 0 or 1 allowed")
+                        f"Task {o.get_id()} already shared block {o.get_shared_block_id()} and cannot share additional block {shared_block_id}. Only 0 or 1 allowed")
                 o.set_shared_block(b)
 
                 # Initialize object user defined data
-                o.get_user_defined()["shared_id"] = b.get_id()
+                o.get_user_defined()["shared_block_id"] = b.get_id()
                 o.get_user_defined()["shared_bytes"] = b.get_size()
                 o.get_user_defined()["home_rank"] = b.get_home_id()
 
