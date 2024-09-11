944c8b6ec67d1dbb988f35d4e9740ca75f3edad8
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-09-11 11:49:55 +0200

    #527: rename and finalized dataset copy script for possible future reuse

---
+++ scripts/copy_dataset.py
@@ -0,0 +1,64 @@
+""" Copy JSON data files from a source directory to a destination directory with optional compression
+Usage:
+- with compression:    `python copy_dataset path/to/src/dir path/to/dst/dir 1`
+- without no compression: `python copy_dataset path/to/src/dir path/to/dst/dir 0`
+"""
+
+import sys
+import os
+import json
+import re
+
+import brotli
+
+def copy_dataset(src_dir: str, dst_dir: str, compress: bool = False)-> int:
+    """Copy JSON data files from a source directory to a destination directory with optional compression"""
+    print("----------------")
+    print("Copy dataset:")
+    print(f"Source dir: {src_dir}")
+    print(f"Dest dir: {dst_dir}")
+    print(f"compress: {compress}")
+    print("----------------")
+
+    if not os.path.isdir(src_dir):
+        raise FileNotFoundError(f"source directory not found at '{src_dir}'")
+    if not os.path.isdir(dst_dir):
+        os.makedirs(dst_dir)
+
+    for i in os.scandir(src_dir):
+        if i.is_file():
+            src_path = os.path.join(src_dir, i.name)
+            dst_path = os.path.join(dst_dir, i.name)
+
+            with open(src_path, "rb") as compr_json_file:
+                compr_bytes = compr_json_file.read()
+                try:
+                    decompr_bytes = brotli.decompress(compr_bytes)
+                    decompressed_dict = json.loads(decompr_bytes.decode("utf-8"))
+                except brotli.error:
+                    decompressed_dict = json.loads(compr_bytes.decode("utf-8"))
+
+            json_str = json.dumps(decompressed_dict, separators=(',', ':'))
+
+            # Issue 527: replace `id` by seq_id in tasks entities and communication nodes
+            # json_str = re.sub(r'"home":([0-9]+),"id":([0-9]+)', r'"home":\1,"seq_id":\2', json_str)
+            # json_str = re.sub(r'"id":([0-9]+),"home":([0-9]+)', r'"seq_id":\1,"home":\2', json_str)
+            # json_str = re.sub(r'"entity":\{"id":([0-9]+),', r'"entity":{"seq_id":\1,', json_str)
+            # json_str = re.sub(r'"home": ([0-9]+),\n([\s]+)"id": ([0-9]+)', r'"home": \1,\n\2"seq_id": \3', json_str)
+            # json_str = re.sub(r'"type":"object","id":([0-9]+)', r'"type":"object","seq_id":\1', json_str)
+
+            print(f"Generating {i.name}...")
+            if compress:
+                print("> Compressing...")
+                with open(dst_path, "wb") as compr_json_file:
+                    compressed_str = brotli.compress(string=json_str.encode("utf-8"), mode=brotli.MODE_TEXT)
+                    compr_json_file.write(compressed_str)
+            else:
+                print("> No compression !")
+                with open(dst_path, "wt", encoding="utf-8") as uncompr_json_file:
+                    uncompr_json_file.write(json_str)
+
+    return 0
+
+if __name__ == "__main__":
+    copy_dataset(sys.argv[1], sys.argv[2], compress=(sys.argv[3] in ['1', 'true', 'True', 'on', 'ON']))
