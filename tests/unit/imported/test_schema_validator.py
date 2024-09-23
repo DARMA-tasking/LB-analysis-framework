76121c3156679b8f507e32cbeed700cb81e61f91
Caleb Schilly <132086024+cwschilly@users.noreply.github.com>
2024-09-23 09:41:56 -0400

    #537: Fix broken CI (#538)
    * #537: update JSON to work with new schema
    
    * #537: collapse all modified JSON into a single line

--- tests/unit/imported/test_schema_validator.py
+++ tests/unit/imported/test_schema_validator.py
@@ -9,16 +9,31 @@ from src.lbaf.Utils.lbsJSONDataFilesValidatorLoader import JSONDataFilesValidato
 JSONDataFilesValidatorLoader().run(overwrite=True)
 from src.lbaf.imported.JSON_data_files_validator import SchemaValidator  # pylint:disable=C0413:wrong-import-position
 
+def decompress_json(input_file):
+    with open(input_file, "rb") as compr_json_file:
+        compr_bytes = compr_json_file.read()
+
+    try:
+        decompr_bytes = brotli.decompress(compr_bytes)
+        json_data = json.loads(decompr_bytes.decode("utf-8"))
+
+    except brotli.error:
+        try:
+            json_data = json.loads(compr_bytes.decode("utf-8"))
+        except json.JSONDecodeError:
+            assert 0, f"Could not decompress {input_file}"
+
+    return json_data
+
 
 class TestConfig(unittest.TestCase):
     def setUp(self):
         self.test_dir = os.path.dirname(os.path.dirname(__file__))
         self.data_dir = os.path.join(self.test_dir, "data", "schema")
+
     def test_schema_validator_valid_001(self):
-        with open(os.path.join(self.data_dir, "valid_schema_001.json"), "rb") as compr_json_file:
-            compr_bytes = compr_json_file.read()
-        decompr_bytes = brotli.decompress(compr_bytes)
-        vjs_json = json.loads(decompr_bytes.decode("utf-8"))
+        schema_file = os.path.join(self.data_dir, "valid_schema_001.json")
+        vjs_json = decompress_json(schema_file)
         is_valid = SchemaValidator(schema_type="LBDatafile").is_valid(schema_to_validate=vjs_json)
         validated_schema = SchemaValidator(schema_type="LBDatafile").validate(schema_to_validate=vjs_json)
         self.assertEqual(vjs_json, validated_schema)
@@ -34,16 +49,14 @@ class TestConfig(unittest.TestCase):
         self.assertEqual(True, is_valid)
 
     def test_schema_validator_invalid_001(self):
-        with open(os.path.join(self.data_dir, "invalid_schema_001.json"), 'r', encoding="utf-8") as invalid_json_schema:
-            vjs_str = invalid_json_schema.read()
-            vjs_json = json.loads(vjs_str)
+        schema_file = os.path.join(self.data_dir, "invalid_schema_001.json")
+        vjs_json = decompress_json(schema_file)
         is_valid = SchemaValidator(schema_type="LBDatafile").is_valid(schema_to_validate=vjs_json)
         self.assertEqual(False, is_valid)
 
     def test_schema_validator_invalid_002(self):
-        with open(os.path.join(self.data_dir, "invalid_schema_002.json"), 'r', encoding="utf-8") as invalid_json_schema:
-            vjs_str = invalid_json_schema.read()
-            vjs_json = json.loads(vjs_str)
+        schema_file = os.path.join(self.data_dir, "invalid_schema_002.json")
+        vjs_json = decompress_json(schema_file)
         is_valid = SchemaValidator(schema_type="LBDatafile").is_valid(schema_to_validate=vjs_json)
         self.assertEqual(False, is_valid)
 
