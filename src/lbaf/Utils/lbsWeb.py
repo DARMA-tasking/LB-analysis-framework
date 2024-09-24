e6d791847947681d776e9799e3d9930b6a71f5d7
Caleb Schilly <cwschilly@gmail.com>
2024-09-24 14:12:22 -0400

    #532: update license copyright years

--- src/lbaf/Utils/lbsWeb.py
+++ src/lbaf/Utils/lbsWeb.py
@@ -5,7 +5,7 @@
 #                                  lbsWeb.py
 #               DARMA/LB-analysis-framework => LB Analysis Framework
 #
-# Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC
+# Copyright 2019-2024 National Technology & Engineering Solutions of Sandia, LLC
 # (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
 # Government retains certain rights in this software.
 #
