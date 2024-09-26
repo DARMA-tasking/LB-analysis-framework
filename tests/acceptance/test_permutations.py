a245a028485916882ca193687d9bb3df98dea664
Caleb Schilly <132086024+cwschilly@users.noreply.github.com>
2024-09-26 12:54:12 -0400

    #547: Add tests for new PrescribedPermutation algorithm (#548)
    * #547: add tests for permutation algorithm
    
    * #547: remove accidental addition
    
    * #547: add capability to call lbaf.run directly
    
    * #547: remove unused imports

---
+++ tests/acceptance/test_permutations.py
@@ -0,0 +1,107 @@
+import os
+import unittest
+
+from src.lbaf.Applications.LBAF_app import LBAFApplication
+
+class TestPermutations(unittest.TestCase):
+    """Class to run acceptance tests"""
+
+    def setUp(self):
+        return
+
+    def tearDown(self):
+        return
+
+    def generate_configuration(self, alpha, beta, gamma, permutation):
+        """Creates and returns the path to a YAML configuration file."""
+        # Determine filepaths
+        acceptance_dir = os.path.dirname(__file__)
+        test_dir = os.path.dirname(acceptance_dir)
+        data_dir = os.path.join(os.path.dirname(test_dir), "data")
+
+        # Create YAML configuration
+        config = {
+            "from_data": {
+                "data_stem": f"{data_dir}/synthetic_lb_data/data",
+                "phase_ids": [0],
+            },
+            "check_schema": False,
+            "work_model": {
+                "name": "AffineCombination",
+                "parameters": {
+                    "alpha": alpha,
+                    "beta": beta,
+                    "gamma": gamma
+                }
+            },
+            "algorithm": {
+                "name": "PrescribedPermutation",
+                "phase_id": 0,
+                "parameters": {
+                    "permutation": permutation
+                }
+            },
+            "logging_level": "info",
+            "output_dir": os.path.join(acceptance_dir, "output"),
+            "output_file_stem": "output_file"
+        }
+
+        # Return the path to the config file
+        return config
+
+    def run_test(self, config, test_case, expected_w_max):
+        """Compare LBAF's results to the expected W_max."""
+        # Determine current directory
+        acceptance_dir = os.path.dirname(__file__)
+
+        # Run LBAF
+        lbaf = LBAFApplication()
+        lbaf.run(cfg=config, cfg_dir=acceptance_dir)
+
+        # Check w_max file exists
+        output_dir = os.path.join(acceptance_dir, "output")
+        imbalance_filepath = os.path.join(output_dir, "imbalance.txt")
+        w_max_filepath = os.path.join(output_dir, "w_max.txt")
+        self.assertTrue(os.path.isfile(w_max_filepath), f"File: {w_max_filepath} does not exist!")
+
+        # Validate w_max value
+        with open(w_max_filepath, 'r', encoding="utf-8") as w_max_file:
+            w_max = float(w_max_file.read())
+            self.assertEqual(w_max, expected_w_max, f"@@@@@ [{test_case}] FOUND W_MAX: {w_max} @@@@@")
+
+        # Clean up
+        os.remove(w_max_filepath)
+        os.remove(imbalance_filepath)
+
+    def test_ccm_permutation(self):
+        # Initialize test cases
+        test_cases = {
+            "load_only": {
+                "alpha": 1.0,
+                "beta": 0.0,
+                "gamma": 0.0,
+                "permutation": dict(enumerate([0, 0, 1, 1, 0, 2, 1, 3, 3])),
+                "W_max": 2.0
+            },
+            "off_node_communication_only": {
+                "alpha": 0.0,
+                "beta": 1.0,
+                "gamma": 0.0,
+                "permutation": dict(enumerate([3, 2, 3, 3, 2, 3, 3, 3, 3])),
+                "W_max": 0.0
+            }
+        }
+
+        # Run each test case
+        for test_case, test_params in test_cases.items():
+            cfg = self.generate_configuration(
+                alpha=test_params["alpha"],
+                beta=test_params["beta"],
+                gamma=test_params["gamma"],
+                permutation=test_params["permutation"]
+            )
+            self.run_test(cfg, test_case, test_params["W_max"])
+
+
+if __name__ == "__main__":
+    unittest.main()
