c824fa3d348979d66b3cc1da49417973e2b2d463
Jonathan Lifflander <jliffla@sandia.gov>
2024-10-21 14:34:17 -0700

    #558: memory: add node-level memory constraint instead of rank-level

---
+++ src/lbaf/Model/lbsNode.py
@@ -0,0 +1,74 @@
+#
+#@HEADER
+###############################################################################
+#
+#                                  lbsNode.py
+#               DARMA/LB-analysis-framework => LB Analysis Framework
+#
+# Copyright 2019-2024 National Technology & Engineering Solutions of Sandia, LLC
+# (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+# Government retains certain rights in this software.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from this
+#   software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Questions? Contact darma@sandia.gov
+#
+###############################################################################
+#@HEADER
+#
+
+import copy
+import math
+import functools
+import operator
+from logging import Logger
+from typing import Optional
+
+class Node:
+    """A class representing a node to which a set of ranks are assigned."""
+
+    def __init__(
+        self,
+        logger: Logger,
+        n_id: int = -1):
+
+        # Assign logger to instance variable
+        self.__logger = logger #pylint:disable=unused-private-member
+
+        # Member variables passed by constructor
+        self.__index = n_id
+        self.__ranks = set()
+
+    def get_max_memory_usage(self):
+        # Combine all memory usages for each rank to get the node memory usage
+        return functools.reduce(operator.add, map(lambda a: a.get_max_memory_usage(), list(self.__ranks)))
+
+    def add_rank(self, rank):
+        self.__ranks.add(rank)
+
+    def get_number_of_ranks(self) -> int:
+        return len(self.__ranks)
