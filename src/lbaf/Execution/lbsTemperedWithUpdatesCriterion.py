6dcf9d8bc0d47bef86182fc18214dc5b4756b82a
Philippe P. Pébaÿ <philippe.pebay@ng-analytics.com>
2026-03-25 12:40:08 -0400

    #613: clarify types in walrus assignments

--- src/lbaf/Execution/lbsTemperedWithUpdatesCriterion.py
+++ src/lbaf/Execution/lbsTemperedWithUpdatesCriterion.py
@@ -68,16 +68,16 @@ class TemperedWithUpdatesCriterion(CriterionBase):
 
         # Compute update formulae
         w_max_up = max(
-            w1 := self._work_model.update(r_src, o_src, o_dst),
-            w2 := self._work_model.update(r_dst, o_dst, o_src))
+            w1 := self._work_model.update(r_src, o_src, o_dst), # type: float
+            w2 := self._work_model.update(r_dst, o_dst, o_src)) # type: float
 
         # Move objects into proposed new arrangement
         self._phase.transfer_objects(r_src, o_src, r_dst, o_dst)
 
         # Compute maximum work of proposed new arrangement
         w_max_new = max(
-            w3 := self._work_model.compute(r_src),
-            w4 := self._work_model.compute(r_dst))
+            w3 := self._work_model.compute(r_src), # type: float
+            w4 := self._work_model.compute(r_dst)) # type: float
 
         # Move objects back into original arrangement
         self._phase.transfer_objects(r_dst, o_src, r_src, o_dst)
